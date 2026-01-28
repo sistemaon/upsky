1656e33d93e8baa3fe3ddee702dd1bca5bbef5c2
eafs <erni.augusto@gmail.com>
2026-01-28 21:08:22 +0000

    update get a quote form

--- forms/quote.php
+++ forms/quote.php
@@ -1,34 +1,37 @@
 <?php
-  $receiving_email_address = 'contact@example.com';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+require '../vendor/autoload.php';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = 'Request for a quote';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $mail = new PHPMailer(true);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    try {
+        // Gmail SMTP Configuration
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'kavallare@gmail.com';
+        $mail->Password = ''; // Generate new app password!
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+        // Email settings
+        $mail->setFrom('kavallare@gmail.com', 'UpSky Quote Form');
+        $mail->addReplyTo($_POST['email'], $_POST['name']);
+        $mail->addAddress('kavallare@gmail.com');
+        $mail->Subject = 'Request for a quote';
 
-  echo $contact->send();
+        $mail->Body = "From: " . $_POST['name'] . "\n";
+        $mail->Body .= "Email: " . $_POST['email'] . "\n";
+        $mail->Body .= "Phone: " . $_POST['phone'] . "\n\n";
+        $mail->Body .= "Message:\n" . $_POST['message'];
+
+        $mail->send();
+        echo 'OK';
+    } catch (Exception $e) {
+        echo "Error: {$mail->ErrorInfo}";
+    }
+}
 ?>
